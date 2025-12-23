436d923c7e1d8ff3cc48ce195eba6b52f71e3067
Deepak <ds262969@gmail.com>
2025-12-23 21:54:04 +0530

    feat(ui): implement BoxList component and integrate active state rendering

--- components/BoxList.tsx
+++ components/BoxList.tsx
@@ -1,3 +1,19 @@
 export default function BoxList() {
-  return <></>;
+  const fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"];
+  return (
+    <div className="w-full absolute z-[-1] top-0 left-0 bg-[#ededed] pt-15 pb-2 rounded-3xl">
+      <div className="flex flex-col gap-3">
+        <ul>
+          {fruits.map((fruit) => (
+            <li
+              key={fruit}
+              className="text-neutral-800 font-medium tracking-tight text-lg px-4 py-2"
+            >
+              {fruit}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
--- components/BoxSelector.tsx
+++ components/BoxSelector.tsx
@@ -1,13 +1,24 @@
+"use client";
 import { ChevronsUpDown } from "lucide-react";
+import { useState } from "react";
+import BoxList from "./BoxList";
 
 export default function BoxSelector() {
+  const [isListActive, setIsListActive] = useState(false);
+
+  const handleListActive = () => {
+    setIsListActive((prev) => !prev);
+  };
   return (
-    <div className="flex justify-between items-center px-3 py-3 bg-neutral-100 text-neutral-800 rounded-2xl border border-gray-300 shadow-sm">
+    <div className="relative flex justify-between items-center px-4 py-3.5 bg-neutral-100 text-neutral-800 w-3xs rounded-2xl shadow-sm">
       <input
-        className="bg-transparent border-none outline-none font-sans font-medium tracking-tight text-lg"
+        onClick={handleListActive}
+        type="text"
+        className="bg-transparent border-none outline-none font-sans font-medium tracking-tight text-xl w-full"
         placeholder="Select a fruit..."
       />
-      <ChevronsUpDown size={"1.1em"} className="text-gray-500 font-bold" />
+      {isListActive && <BoxList />}
+      <ChevronsUpDown className="text-gray-500 font-bold w-5 h-5" />
     </div>
   );
 }
