da606968e48286e7078ef2ca865cadc89095ddf0
Deepak <ds262969@gmail.com>
2026-01-03 10:35:02 +0530

    Finalizing the details

--- components/BoxSelector.tsx
+++ components/BoxSelector.tsx
@@ -2,17 +2,11 @@
 import { ChevronsUpDown } from "lucide-react";
 import { motion, Transition } from "motion/react";
 import { useEffect, useRef, useState } from "react";
-import BoxList from "./BoxList";
 
 export default function BoxSelector() {
-  const [isListActive, setIsListActive] = useState(false);
   const [value, setValue] = useState("");
   const [caretOffset, setCaretOffset] = useState(0);
   const spanRef = useRef<HTMLSpanElement>(null);
-
-  const handleListActive = () => {
-    setIsListActive((prev) => !prev);
-  };
 
   useEffect(() => {
     if (spanRef.current) {
@@ -23,8 +17,8 @@ export default function BoxSelector() {
 
   const CARET_TRANSITION: Transition = {
     duration: 0.125,
-    ease: "easeIn"
-  }
+    ease: "easeIn",
+  };
 
   return (
     <div className="relative z-40">
@@ -36,8 +30,6 @@ export default function BoxSelector() {
           {value}
         </span>
         <input
-          onClick={handleListActive}
-          // onBlur={() => setIsListActive(false)}
           onChange={(e) => setValue(e.target.value)}
           type="text"
           className="relative bg-transparent border-none outline-none font-sans font-medium tracking-tight text-xl w-full caret-transparent"
@@ -48,7 +40,6 @@ export default function BoxSelector() {
           transition={CARET_TRANSITION}
           className="absolute w-0.75 h-7 bg-[#ff5400] rounded-[3px] pointer-events-none animate-pulse"
         ></motion.span>
-        {isListActive && <BoxList />}
         <ChevronsUpDown className="text-gray-500 font-bold w-5 h-5" />
       </div>
     </div>
